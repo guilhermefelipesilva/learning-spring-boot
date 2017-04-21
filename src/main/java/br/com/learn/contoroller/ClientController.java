9dd16877abce2c781592a8b1cc5a105559d644ca
guilhermefelipesilva <guilhermefelipesilva@gmail.com>
2017-04-21 11:23:39 -0300

    Controller refactoring to consume service

--- src/main/java/br/com/learn/contoroller/ClientController.java
+++ src/main/java/br/com/learn/contoroller/ClientController.java
@@ -1,8 +1,7 @@
 package br.com.learn.contoroller;
 
 import java.util.Collection;
-import java.util.HashMap;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,17 +12,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import br.com.learn.models.Client;
+import br.com.learn.service.ClientService;
 
 @RestController
 public class ClientController {
 
-	HashMap<Integer, Client> clients = new HashMap<>();
-	Integer proximoId = 1;
+	@Autowired
+	ClientService clientService;
 
 	@RequestMapping(method = RequestMethod.POST, value = "/clients", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Client> cadastrarClient(@RequestBody Client client) {
 
-		Client clienteCadastrado = cadastrar(client);
+		Client clienteCadastrado = clientService.cadastrar(client);
 
 		return new ResponseEntity<Client>(clienteCadastrado, HttpStatus.CREATED);
 	}
@@ -31,7 +31,7 @@ public class ClientController {
 	@RequestMapping(method = RequestMethod.GET, value = "/clients", produces = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Collection<Client>> buscarClients() {
 
-		Collection<Client> clientsBuscados = buscarClientsCadastrados();
+		Collection<Client> clientsBuscados = clientService.buscarClientsCadastrados();
 
 		return new ResponseEntity<>(clientsBuscados, HttpStatus.OK);
 	}
@@ -39,13 +39,13 @@ public class ClientController {
 	@RequestMapping(method = RequestMethod.DELETE, value = "/clients/{id}")
 	public ResponseEntity<Client> removerClients(@PathVariable Integer id) {
 
-		Client client = buscarPorId(id);
+		Client client = clientService.buscarPorId(id);
 		
 		if (client==null){
 			return new ResponseEntity<>(HttpStatus.NOT_FOUND);
 		}
 		
-		excluir(client);
+		clientService.excluir(client);
 		
 		return new ResponseEntity<>(HttpStatus.OK);
 	}
@@ -53,36 +53,11 @@ public class ClientController {
 	@RequestMapping(method = RequestMethod.PUT, value = "/clients", consumes = MediaType.APPLICATION_JSON_VALUE)
 	public ResponseEntity<Client> calterarClient(@RequestBody Client client) {
 
-		Client clienteAlterado = alterar(client);
+		Client clienteAlterado = clientService.alterar(client);
 
 		return new ResponseEntity<Client>(clienteAlterado, HttpStatus.OK);
 	}
 	
-	private Client alterar(Client client){
-		clients.put(client.getId(), client);
-		return client;
-	}
 
-	private Client buscarPorId(Integer id) {
-		return clients.get(id);
-	}
-
-	private void excluir(Client client) {
-		clients.remove(client.getId());		
-	}
-
-	private Collection<Client> buscarClientsCadastrados() {
-
-		return clients.values();
-	}
-
-	private Client cadastrar(Client client) {
-
-		client.setId(proximoId);
-		proximoId++;
-
-		clients.put(client.getId(), client);
-
-		return client;
-	}
+	
 }
